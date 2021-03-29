02c39f4e1f1f471f8450808b34bda1395ea7c3b2
fsolerl <fsoler@globaltask.net>
2021-03-28 22:07:16 -0300

    Interaction and Dialogue Systems

---
+++ Assets/Scripts/Interaction/Actor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Data/Actor")]
+public class Actor : ScriptableObject
+{
+    public string Name;
+    public Texture2D characterPortrait;
+}
+//Scriptable object that stores information about an NPC
---
+++ Assets/Scripts/Interaction/NPCInteraction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class NPCInteraction : Interactable
+{
+    [SerializeField] Dialogue dialogue;
+
+    public override void Interact()
+    {
+        TriggerDialogue();
+    }
+
+    private void TriggerDialogue()
+    {
+        //Checking if canvas is active, if it is then play next dialog, if not then start it
+        if(dialogue != null)
+        {
+            if(DialogueManager.instance.canvas.activeInHierarchy == true)
+            {
+                DialogueManager.instance.Next();
+            }
+            else
+            {
+                DialogueManager.instance.StartDialogue(dialogue);
+            }
+        }
+    }
+}
