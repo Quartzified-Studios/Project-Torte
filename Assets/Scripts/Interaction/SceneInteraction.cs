fefdae2ebf9b276c3e6b5ca30daeca2751760aa5
realQuartzi <nightmaresdev@gmail.com>
2021-04-06 14:22:38 +0200

    New Map & More
    Updated Sprites and Map
    Included Scene Switch Code

---
+++ Assets/Scripts/Interaction/SceneInteraction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneInteraction : Interactable
+{
+    public string sceneName;
+
+    public override void Interact()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
